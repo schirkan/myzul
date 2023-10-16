95fb43b74eb98e93ecf9bdd42175350f49c90ccc
schirkan <schirkan@googlemail.com>
2023-10-16 20:37:50 +0000

    offline

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import 'the-new-css-reset/css/reset.css'
 import { ThemeProvider } from './common/ThemeContext';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
@@ -14,6 +15,8 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
